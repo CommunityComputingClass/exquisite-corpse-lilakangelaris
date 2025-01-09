836d9dc4185acd42b979781cc27b5c4684661494
lilakangelaris <125599089+lilakangelaris@users.noreply.github.com>
2025-01-09 17:53:18 +0000

    submitting my first code

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,13 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
 
 function draw() {
-  background(200);
-  
+  background(143, 206, 255);
+  angleMode(DEGREES)
+ // CHORD fill mode.
+ arc(180, 200, 100, 180, 0, 180)
+ arc(290, 200, 100, 180, 0, 180)
+ arc(180, 200, 100, 140, 180, 360)
+ arc(290, 200, 100, 140, 180, 360)
 }
